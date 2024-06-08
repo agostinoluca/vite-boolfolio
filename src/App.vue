aaccf2b211d13a347cf497a55accc068feced4e3
LUCA AGOSTINO <agostino.luca89@gmail.com>
2024-06-08 20:06:46 +0200

    add api call add jumbotron add search bar add method for search a project

--- src/App.vue
+++ src/App.vue
@@ -7,27 +7,65 @@ export default {
         return {
             base_api_url: 'http://127.0.0.1:8000',
             projects_endpoint: '/api/projects',
-            projects: null
+            projects: null,
+            search_project: ''
         }
+    },
+    methods: {
+        search() {
+            const projects_url = this.base_api_url + this.projects_endpoint + `?search=${this.search_project}`
+            console.log(projects_url);
+            this.callApi(projects_url)
+        },
+
+        callApi(projects_url) {
+            axios
+                .get(projects_url)
+                .then(resp => {
+                    console.log(resp);
+                    this.projects = resp.data.results
+                })
+                .catch(err => {
+                    console.error(err)
+                })
+        }
+
     },
     mounted() {
         const projects_url = this.base_api_url + this.projects_endpoint
         console.log(projects_url);
-        axios
-            .get(projects_url)
-            .then(resp => {
-                console.log(resp);
-                this.projects = resp.data.results
-            })
-            .catch(err => {
-                console.error(err)
-            })
+        this.callApi(projects_url)
     }
 }
 </script>
 
 <template>
-    <h1>Projects</h1>
+
+    <div class="p-2 mb-4 bg-secondary">
+        <div class="container-fluid py-5 text-light">
+            <h1 class="display-5 fw-bold">Browse My Web Development Projects!</h1>
+            <p class="col-md-8 fs-4 p-2">
+                I'm Luca, a passionate and creative web developer. Explore my projects to see how i transform
+                innovative ideas into extraordinary digital experiences. From responsive websites to interactive web
+                applications, each project showcases my dedication to intuitive design and flawless functionality.
+
+                Scroll down to see my work and get inspired!
+            </p>
+
+            <form @submit.prevent="search()">
+                <div class="input-group mb-3">
+                    <input type="search" class="form-control" placeholder="search..." v-model="search_project">
+                    <button class="btn btn-outline-light" type="submit">
+                        <i class="fas fa-search fa-lg fa-fw"></i>
+                    </button>
+                </div>
+            </form>
+
+
+        </div>
+    </div>
+
+
     <section class="projects" v-if="projects">
         <div class="container">
             <div class="row row-cols-1 row-cols-sm-2 row-cols-sm-3 g-4">
